bd02906e72dd952a1be70fd79506dd4dc622de15
Maksim Andreev <andreev.kras@gmail.com>
2017-02-07 15:50:01 +0700

    убрал движение анимации
    теперь не двигается влево-вправо, просто стоит на месте, пока меняются
    блоки

--- js/scroll-safedrive.js
+++ js/scroll-safedrive.js
@@ -8,214 +8,14 @@
                 triggerElement: '#safedrive-parent',
                 triggerHook: 0
             })
-            .setClassToggle('#safedrive', 'position1')
             .addTo(controller);
 
-            var safedriveimageMove001 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '20%'
-            })
-            .setClassToggle('#safedrive', 'position11')
-            .addTo(controller);
-
-            var safedriveimageMove002 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '40%'
-            })
-            .setClassToggle('#safedrive', 'position12')
-            .addTo(controller);
-
-            var safedriveimageMove003 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '60%'
-            })
-            .setClassToggle('#safedrive', 'position13')
-            .addTo(controller);
-
-            var safedriveimageMove004 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '80%'
-            })
-            .setClassToggle('#safedrive', 'position14')
-            .addTo(controller);
-
-            var safedriveimageMove01 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '100%'
-            })
-            .setClassToggle('#safedrive', 'position2')
-            .addTo(controller);
-
-            var safedriveimageMove011 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '120%'
-            })
-            .setClassToggle('#safedrive', 'position21')
-            .addTo(controller);
-
-            var safedriveimageMove012 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '140%'
-            })
-            .setClassToggle('#safedrive', 'position22')
-            .addTo(controller);
-
-            var safedriveimageMove013 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '160%'
-            })
-            .setClassToggle('#safedrive', 'position23')
-            .addTo(controller);
-
-            var safedriveimageMove014 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '180%'
-            })
-            .setClassToggle('#safedrive', 'position24')
-            .addTo(controller);
-
-            var safedriveimageMove02 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '200%'
-            })
-            .setClassToggle('#safedrive', 'position3')
-            .addTo(controller);
-
-            var safedriveimageMove021 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '220%'
-            })
-            .setClassToggle('#safedrive', 'position31')
-            .addTo(controller);
-
-            var safedriveimageMove022 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '240%'
-            })
-            .setClassToggle('#safedrive', 'position32')
-            .addTo(controller);
-
-            var safedriveimageMove023 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '260%'
-            })
-            .setClassToggle('#safedrive', 'position33')
-            .addTo(controller);
-
-            var safedriveimageMove024 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '280%'
-            })
-            .setClassToggle('#safedrive', 'position34')
-            .addTo(controller);
-
-            var safedriveimageMove03 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '300%'
-            })
-            .setClassToggle('#safedrive', 'position4')
-            .addTo(controller);
-
-            var safedriveimageMove031 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '320%'
-            })
-            .setClassToggle('#safedrive', 'position41')
-            .addTo(controller);
-
-            var safedriveimageMove032 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '340%'
-            })
-            .setClassToggle('#safedrive', 'position42')
-            .addTo(controller);
-
-            var safedriveimageMove033 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '360%'
-            })
-            .setClassToggle('#safedrive', 'position43')
-            .addTo(controller);
-
-            var safedriveimageMove034 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '380%'
-            })
-            .setClassToggle('#safedrive', 'position44')
-            .addTo(controller);
-
-            var safedriveimageMove04 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '400%'
-            })
-            .setClassToggle('#safedrive', 'position5')
-            .addTo(controller);
-
-            var safedriveimageMove001 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '420%'
-            })
-            .setClassToggle('#safedrive', 'position51')
-            .addTo(controller);
-
-            var safedriveimageMove002 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '440%'
-            })
-            .setClassToggle('#safedrive', 'position52')
-            .addTo(controller);
-
-            var safedriveimageMove003 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '460%'
-            })
-            .setClassToggle('#safedrive', 'position53')
-            .addTo(controller);
-
-            var safedriveimageMove004 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '480%'
-            })
-            .setClassToggle('#safedrive', 'position54')
-            .addTo(controller);
-
-            var safedriveimageMove05 = new ScrollMagic.Scene({
-                triggerElement: '#safedrive-parent',
-                triggerHook: 'onLeave',
-                offset: '500%'
-            })
-            .setClassToggle('#safedrive', 'position6')
-            .addTo(controller);
 
             //showing and hiding messages
 
             var safedrivemessageShow00 = new ScrollMagic.Scene({
                 triggerElement: '#safedrive-parent',
-                duration: '15%',
+                duration: '14%',
                 triggerHook: 'onLeave',
                 offset: '150%'
             })
@@ -233,9 +33,9 @@
 
             var safedrivemessageShow01 = new ScrollMagic.Scene({
                 triggerElement: '#safedrive-parent',
-                duration: '15%',
+                duration: '14%',
                 triggerHook: 'onLeave',
-                offset: '250%'
+                offset: '270%'
             })
             .setClassToggle('#safedrive-message-01', 'safedrive-message-shown')
             .addTo(controller);
@@ -252,18 +52,18 @@
 
             var safedrivemessageShow02 = new ScrollMagic.Scene({
                 triggerElement: '#safedrive-parent',
-                duration: '15%',
+                duration: '14%',
                 triggerHook: 'onLeave',
-                offset: '350%'
+                offset: '390%'
             })
             .setClassToggle('#safedrive-message-02', 'safedrive-message-shown')
             .addTo(controller);
 
             var safedrivemessageShow03 = new ScrollMagic.Scene({
                 triggerElement: '#safedrive-parent',
-                duration: '15%',
+                duration: '14%',
                 triggerHook: 'onLeave',
-                offset: '450%'
+                offset: '490%'
             })
             .setClassToggle('#safedrive-message-03', 'safedrive-message-shown')
             .addTo(controller);
@@ -272,7 +72,7 @@
                 triggerElement: '#safedrive-parent',
                 duration: '23%',
                 triggerHook: 'onLeave',
-                offset: '550%'
+                offset: '590%'
             })
             .setClassToggle('#safedrive-message-04', 'safedrive-message-shown')
             .addTo(controller);
